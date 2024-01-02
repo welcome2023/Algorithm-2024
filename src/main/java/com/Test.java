3e72225fafd453a972f4f06c738cbfe29d885ec3
cms <cmsxyz@163.com>
2024-01-03 00:02:34 +0800

    选择排序-位运算符

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -8,6 +8,33 @@ import java.util.HashSet;
  */
 public class Test {
     public static void main(String[] args) {
-
+        int[] arr = {7,3,1,2,6};
+        sort(arr);
+        for (int i : arr) {
+            System.out.println(i);
+        }
     }
+
+    private static void sort(int[] arr) {
+        if(arr==null||arr.length<2){
+            return;
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i-1; j >=0&&arr[j]>arr[j+1]; j--) {
+                swap(arr,j,j+1);
+
+            }
+
+        }
+    }
+
+
+    public static void swap(int []arr,int i,int j){
+        int tmp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=tmp;
+    }
+
+
 }
