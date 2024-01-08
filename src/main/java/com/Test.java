8a1a0a3c313532eb80af7a520bdff3024d27a183
cms <cmsxyz@163.com>
2024-01-08 22:45:35 +0800

    小和问题

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -1,24 +1,47 @@
 package com;
 
 
+
 /**
  * @createDate 2023-12-25 22:52
  */
 public class Test {
     public static void main(String[] args) {
-        int []arr={1,4,2,3,6,7,8,99,100};
-        System.out.println(process(arr, 0, arr.length - 1));
+        int[]arr={1,2,4,6,2,4,5,3,2};
+        process(arr,0,arr.length-1);
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+    public static void  process(int []arr,int l,int r){
+        if(l==r){
+            return;
+        }
+
+        int mid=l+((r-l)>>1);
+        process(arr,l,mid);
+        process(arr,mid+1,r);
+
+        merge(arr,l,mid,r);
+
     }
 
-   public static int process(int []arr,int l,int r){
-       if(l==r){
-           return arr[l];
-       }
-
-       int mid =l+((r-l)>>1);
-       int leftMax=process(arr,l,mid);
-       int rightMax=process(arr,mid+1,r);
-       return Math.max(leftMax,rightMax);
-   }
-
+    private static void merge(int[] arr, int l, int m, int r) {
+        int p1=l;
+        int p2=m+1;
+        int i=0;
+        int []help=new int[r-l+1];
+        while (p1<=m&&p2<=r){
+            help[i++]=arr[p1]<=arr[p2]?arr[p1++]:arr[p2++];
+        }
+        while (p1<=m){
+            help[i++]=arr[p1++];
+        }
+        while (p2<=r){
+            help[i++]=arr[p2++];
+        }
+        for ( i = 0; i < help.length; i++) {
+            arr[l+i]=help[i];
+        }
+    }
 }
---
+++ src/main/java/com/algorithmtest/recursion/SmallSum.java
@@ -0,0 +1,43 @@
+package com.algorithmtest.recursion;
+
+/**
+ * @createDate 2024-01-08 21:44
+ * 小和问题:在一个数组中,每一个数左边比当前数小的数累加起来,叫做这个数组的小和.
+ */
+public class SmallSum {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 2, 5};
+        System.out.println(process(arr, 0, arr.length - 1));
+    }
+
+    public static int process(int[] arr, int l, int r) {
+        if (l == r) {
+            return 0;
+        }
+        int mid = l + ((r - l) >> 1);
+        return process(arr, l, mid) + process(arr, mid + 1, r) + merge(arr, l, mid, r);
+    }
+
+    private static int merge(int[] arr, int l, int m, int r) {
+        int[] help = new int[r - l + 1];
+        int i = 0;
+        int p1 = l;
+        int p2 = m + 1;
+        int res = 0;
+        while (p1 <= m && p2 <= r) {
+            res += arr[p1] < arr[p2] ? (r - p2 + 1) * arr[p1] : 0;
+            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= m) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[l + i] = help[i];
+        }
+        return res;
+    }
+
+}
