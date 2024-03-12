637fe214cf1d7836d051632a9ea9a60d8b98be7e
cms <cmsxyz@163.com>
2024-03-12 21:16:53 +0800

    作业描述:第一阶段结束!!!

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -1,12 +1,16 @@
 package com;
 
 
+import java.sql.SQLOutput;
+
 /**
  * @createDate 2023-12-25 22:52
  * getMax
  */
 public class Test {
-
+    public static void main(String[] args) {
+        System.out.println("第一阶段结束,撒花!!!!!!");
+    }
 
 }
 
