3ae793a68a54d7b0b1e066df82c4c620828d2cb7
cms <cmsxyz@163.com>
2024-03-21 22:34:13 +0800

    【华为】回溯,贪心,优先队列刷完

---
+++ src/main/java/com/hwtest/greedy/MaxArea.java
@@ -0,0 +1,38 @@
+package com.hwtest.greedy;
+
+import javax.swing.*;
+
+/**
+ * @author cmsxyz@163.com
+ * @date 2024-03-21 22:03
+ * @usage
+ */
+public class MaxArea {
+    public static void main(String[] args) {
+        System.out.println(maxArea2(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    }
+
+    public static int maxArea1(int[] height) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                max = Math.max((j - i) * Math.min(height[i], height[j]), max);
+            }
+        }
+        return max;
+    }
+
+
+    public static int maxArea2(int[] height) {
+        int max = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            max = Math.max((r - l) * Math.min(height[l], height[r]), max);
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return max;
+    }
+}
