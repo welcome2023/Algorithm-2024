b3b586d2b43e254b05f12a4b1f9a78a8cdbc9f28
cms <cmsxyz@163.com>
2024-05-11 17:28:10 +0800

    【华为】撒花,再次撒花!!!

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -1,80 +1,12 @@
 package com;
 
-import java.util.*;
-
 /**
  * @createDate 2023-12-25 22:52
  * getMax
  */
 
 public class Test {
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int[] arr = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
-        List<Tree2> list = new ArrayList<>();
-
-        for (int i : arr) {
-            Tree2 tree = new Tree2(i, 1);
-            list.add(tree);
-        }
-
-        while (list.size() > 1) {
-            Collections.sort(list);
-            Tree2 first = list.remove(0);
-            Tree2 second = list.remove(0);
-            int maxHeight=Math.max(first.height,second.height);
-            Tree2 tree = new Tree2(first, second, first.value + second.value, maxHeight + 1);
-            list.add(tree);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        dfs(sb,list.get(0));
-        System.out.print(sb);
-
-
-    }
-
-    private static void dfs(StringBuilder sb, Tree2 tree) {
-        if(tree==null){
-            return;
-        }
-        dfs(sb,tree.left);
-        sb.append(tree.value).append(" ");
-        dfs(sb,tree.right);
-    }
-}
-
-// 按照要求定义二叉树
-
-class Tree2 implements Comparable<Tree2> {
-    int value;
-    Tree2 left;
-    Tree2 right;
-    int height;
-
-    public Tree2(int value) {
-        this.value = value;
-    }
-
-    public Tree2(int value, int hei) {
-        this.value = value;
-        this.height = hei;
-    }
-
-    public Tree2(Tree2 left,Tree2 right,int value,int height){
-        this.left=left;
-        this.right=right;
-        this.value=value;
-        this.height=height;
-    }
-
-
-    @Override
-    public int compareTo(Tree2 o) {
-        return o.value == this.value ? this.height - o.height : this.value - o.value;
-    }
 }
 
 
--- src/main/java/com/realpractice/day12/coding/Class01_Tree_Huffman.java
+++ src/main/java/com/realpractice/day12/coding/Class01_Tree_Huffman.java
@@ -1,6 +1,6 @@
 package com.realpractice.day12.coding;
 
-import java.util.*;
+import java.util.Scanner;
 
 /**
  * @author cmsxyz@163.com
@@ -10,68 +10,14 @@ import java.util.*;
 public class Class01_Tree_Huffman {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = 5;
-        String[] strings = "5 15 40 30 10".split(" ");
-        int[] ints = Arrays.stream(strings).mapToInt(Integer::parseInt).toArray();
-        List<Tree> treeList = new ArrayList<>();
+        int n = sc.nextInt();
+        int profit=0;
+        int duan=0;
 
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
 
-        for (int anInt : ints) {
-            Tree tree = new Tree(anInt, 1);
-            treeList.add(tree);
-        }
-        while (treeList.size() > 1) {
-            //根据 val 进行排序，每次循环进行排序可以保证左树轻于右树
-            Collections.sort(treeList);
-            Tree first = treeList.remove(0);
-            Tree second = treeList.remove(0);
-            int maxHeight = Math.max(first.height, second.height);
-            Tree tree = new Tree(first, second, first.val + second.val, maxHeight + 1);
-            treeList.add(tree);
-        }
-        StringBuilder stringBuilder = new StringBuilder();
-        dfs(stringBuilder, treeList.get(0));
-        System.out.println(stringBuilder);
-    }
-
-
-
-    public static void dfs(StringBuilder res, Tree root) {
-        if (root == null) {
-            return;
-        }
-        //按照 左-打印-右的方式遍历
-        dfs(res, root.left);
-        res.append(root.val + " ");
-        dfs(res, root.right);
-    }
-
-    static class Tree implements Comparable<Tree> {
-        private Tree left;
-        private Tree right;
-        private int val;
-        private int height;
-
-        public Tree(int val, int height) {
-            this.val = val;
-            this.height = height;
-        }
-
-        public Tree(Tree left, Tree right, int val, int height) {
-            this.left = left;
-            this.right = right;
-            this.val = val;
-            this.height = height;
-        }
-
-        @Override
-        public int compareTo(Tree tree) {
-
-            if (tree.val == this.val) {
-                return this.height - tree.height;
             }
-
-            return this.val - tree.val;
         }
     }
 }
